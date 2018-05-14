9f47e947a4f38eb2924cdb9d32b61a7fd5758e4d
Naveen <naveen.w20@gmail.com>
2018-05-14 15:33:40 -0500

    uploaded the test files

---
+++ Ui naveen.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+	function reverseString(str)
+	{
+	return str;
+	}
+	reverseString("naveen");
+
+</script>
+</body>
+</html>
---
+++ angular js/typescript.ts
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="UTF-8">
+ 	<title>Title</title>
+
+ 	<script>
+ 	  var a=10;
+ 	  var b=23.33;
+
+ 	  console.log(a);
+ 	  console.log(b);
+ 	  </script>
+ 	  <script src="primitivedatatypes.js"></script>
+ 	  </head>
+ 	  <body>
+ 	  </body>
+ 	  </html>
+ 	  
